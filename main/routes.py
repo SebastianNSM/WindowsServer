3fc8ace66b9ad3e7edc7af77c8e2c1753181bbc0
Sebastian Soto <dev.snsm@gmail.com>
2020-08-17 00:25:10 -0600

    added documentation to be downloaded

--- main/routes.py
+++ main/routes.py
@@ -3,9 +3,15 @@ from flask import Blueprint, redirect, render_template
 
 r_base = Blueprint('r_base', __name__, static_folder='static')
 
+
 @r_base.route('/')
 def index():
     vp = 'videos.json'
     with open(vp, encoding='utf-8') as data_file:
         data = json.loads(data_file.read())
     return render_template('views/base/index.html', videos=data)
+
+
+@r_base.route('/get_docs')
+def get_documentation():
+    return r_base.send_static_file('grupo2.zip')
